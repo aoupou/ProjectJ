979351746183e21702fc479d96c0a5b04a471b24
aoupou <ooaaaapppp@gmail.com>
2026-09-10 00:04:06 +0900

    회전 및 스피드 구현
    현재 고민 : OnMove--를 한 함수로 묶어서 코드를 읽기 편하게 할 수 있지안을까...? 차피 중복한 내용이 들어간다면. .. . .

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -3,23 +3,28 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 1;
     public void OnMoveUp(InputValue value)
     {
-        transform.position += Vector3.up;
+        transform.position += Vector3.up * speed;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
     public void OnMoveDown(InputValue value)
     {
-        transform.position += Vector3.down;
+        transform.position += Vector3.down * speed;
+        transform.rotation = Quaternion.Euler(0, 0, 180);
     }
 
     public void OnMoveLeft(InputValue value)
     {
-        transform.position += Vector3.left;
+        transform.position += Vector3.left * speed;
+        transform.rotation = Quaternion.Euler(0, 0, 90);
     }
 
     public void OnMoveRight(InputValue value)
     {
-        transform.position += Vector3.right;
+        transform.position += Vector3.right * speed;
+        transform.rotation = Quaternion.Euler(0, 0, 270);
     }
 }
